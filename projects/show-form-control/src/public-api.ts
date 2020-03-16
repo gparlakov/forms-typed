0fc4ea9dac269687db3b223ffca42392c0a29ad2
Georgi Parlakov <gparlakov@gmail.com>
2020-03-16 15:54:39 +0200

    npx-show-form-control package

---
+++ projects/show-form-control/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of show-form-control
+ */
+
+export * from './lib/show-form-control/show-form-control';
+export * from './lib/show-form-control/show-form-control.component';
+export * from './lib/show-form-control/show-form-control.module';
